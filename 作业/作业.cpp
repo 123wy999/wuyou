caa6700d15c81685a8440085d413be15058caf1a
123wy999 <115570158+123wy999@users.noreply.github.com>
2022-10-11 23:27:38 +0800

    Add files via upload

---
+++ 作业/作业.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	int 数字 = 0;
+	cout << "请输入数字：  " << endl;
+	cin >> 数字;
+
+	switch (数字)
+	{
+	case 1:
+		cout << "Hello RoBoMaster" << endl;
+		break;
+
+	case 2:
+		cout << "Hello RoBoMaster  Hello RoBoMaster" << endl;
+		break;
+
+	case 3:
+		cout << "Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster" << endl;
+		break;
+
+	case 4:
+		cout << "Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster" << endl;
+		break;
+
+	case 5:
+		cout << "Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster " << endl;
+		break;
+
+	case 6:
+		cout << "Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  " << endl;
+		break;
+
+	case 7:
+		cout << "Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster" << endl;
+		break;
+
+	case 8:
+		cout << "Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster" << endl;
+		break;
+
+
+	case 9:
+		cout << "Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster" << endl;
+		break;
+
+
+	case 10:
+		cout << "Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  Hello RoBoMaster  " << endl;
+		break;
+
+
+	}
+	system("pause");
+}
